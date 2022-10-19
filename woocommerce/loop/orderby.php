26cb0a7cb472927c37e44b9d92b59b66e7624a52
Kostas Vrouvas <kosvrouvas@gmail.com>
2022-10-19 13:27:28 +0300

    Update orderby.php

--- woocommerce/loop/orderby.php
+++ woocommerce/loop/orderby.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <form class="woocommerce-ordering" method="get">
-	<label style="display:none;" for="orderby" title="Order">Order by</label>
+	<label class="screen-reader-text" for="orderby" title="Order">Order by</label>
 	<select id="orderby" name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
 		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
 			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
